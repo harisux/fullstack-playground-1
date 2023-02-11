4a5b90e8566db6c401e9a37643259ea4889cfc02
harisux <haris.chaudhrym1@gmail.com>
2023-02-11 02:31:27 -0500

    refactor: :recycle: renamed front project

--- frontend-solution-fs1/src/app/app.component.ts
+++ frontend-solution-fs1/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend-solution-1';
+  title = 'frontend-solution-fs1';
 }
