aec36ec92ee3895c00df66cb87577eb2b5bb63be
harisux <haris.chaudhrym1@gmail.com>
2023-02-12 14:06:59 -0500

    feat: :construction: initialized pd1 backend solution bs1 project! + working as eureka client

---
+++ problem-domain-pd1/pd1-backend-solution-bs1/src/main/java/org/harisux/fullstackplay/pd1backendsolutionbs1/Pd1BackendSolutionBs1Application.java
@@ -0,0 +1,13 @@
+package org.harisux.fullstackplay.pd1backendsolutionbs1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pd1BackendSolutionBs1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pd1BackendSolutionBs1Application.class, args);
+	}
+
+}
---
+++ problem-domain-pd1/pd1-backend-solution-bs1/src/test/java/org/harisux/fullstackplay/pd1backendsolutionbs1/Pd1BackendSolutionBs1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.harisux.fullstackplay.pd1backendsolutionbs1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pd1BackendSolutionBs1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
