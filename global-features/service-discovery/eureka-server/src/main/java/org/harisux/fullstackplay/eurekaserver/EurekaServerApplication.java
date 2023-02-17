c5fe94a0e4a0b33db88812e5487c9bbe4a30bc51
harisux <haris.chaudhrym1@gmail.com>
2023-02-17 00:56:09 -0500

    feat: :adhesive_bandage: disabling cors and csrf for eureka server

--- global-features/service-discovery/eureka-server/src/main/java/org/harisux/fullstackplay/eurekaserver/EurekaServerApplication.java
+++ global-features/service-discovery/eureka-server/src/main/java/org/harisux/fullstackplay/eurekaserver/EurekaServerApplication.java
@@ -1,15 +1,41 @@
 package org.harisux.fullstackplay.eurekaserver;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableWebSecurity
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("*"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+		return http
+			.cors().configurationSource(source).and()
+			.csrf().disable()
+			.build();
 	}
 
 }
