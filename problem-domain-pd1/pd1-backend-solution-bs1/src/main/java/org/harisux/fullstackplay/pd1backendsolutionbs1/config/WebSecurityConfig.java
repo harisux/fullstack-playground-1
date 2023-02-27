d702580d94ca90bd02152a315f2d62c549a3557d
harisux <haris.chaudhrym1@gmail.com>
2023-02-27 01:27:42 -0500

    feat: :sparkles: adding config to disable cors

---
+++ problem-domain-pd1/pd1-backend-solution-bs1/src/main/java/org/harisux/fullstackplay/pd1backendsolutionbs1/config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package org.harisux.fullstackplay.pd1backendsolutionbs1.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    
+    //Disables cors
+    @Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("*"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+		return http
+			.cors().configurationSource(source).and()
+			.csrf().disable()
+			.build();
+	}
+
+}
