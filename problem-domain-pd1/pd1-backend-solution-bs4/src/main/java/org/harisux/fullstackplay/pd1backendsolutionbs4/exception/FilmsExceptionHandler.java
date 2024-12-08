10a3ad7089b4f0154a325e27ad31cb3af9785b8e
harisux <haris.chaudhrym1@gmail.com>
2024-12-08 12:23:57 -0500

    feat: :sparkles: adding global error handling for reactive v1

--- problem-domain-pd1/pd1-backend-solution-bs4/src/main/java/org/harisux/fullstackplay/pd1backendsolutionbs4/exception/FilmsExceptionHandler.java
+++ problem-domain-pd1/pd1-backend-solution-bs4/src/main/java/org/harisux/fullstackplay/pd1backendsolutionbs4/exception/FilmsExceptionHandler.java
@@ -3,38 +3,31 @@ package org.harisux.fullstackplay.pd1backendsolutionbs4.exception;
 import org.harisux.fullstackplay.pd1backendsolutionbs4.exception.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.handler.ResponseStatusExceptionHandler;
 
-import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
 
 @ControllerAdvice
-@Slf4j
-public class FilmsExceptionHandler {
+public class FilmsExceptionHandler extends ResponseStatusExceptionHandler {
     
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleException(FilmNotFoundException exception) {
-        log.error("Film not found error!", exception);
-        return new ResponseEntity<>(
-            new ErrorResponse("ERR001", exception.getMessage()), 
-            HttpStatus.NOT_FOUND);
+    @ExceptionHandler(FilmNotFoundException.class)
+    public Mono<ResponseEntity<ErrorResponse>> handleException(FilmNotFoundException exception) {
+        return Mono.just(
+            new ResponseEntity<>(
+                new ErrorResponse("ERR001", exception.getMessage()), 
+                HttpStatus.NOT_FOUND)
+        );
     }
 
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleException(MissingServletRequestParameterException exception) {
-        log.error("Missing parameter!", exception);
-        return new ResponseEntity<ErrorResponse>(
-            new ErrorResponse("ERR002", exception.getMessage()),
-            HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleException(Exception exception) {
-        log.error("Default error!", exception);
-        return new ResponseEntity<ErrorResponse>(
-            new ErrorResponse("ERR000", "Something went wrong..."),
-            HttpStatus.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler(Exception.class)
+    public Mono<ResponseEntity<ErrorResponse>> handleException(Exception exception) {
+        return Mono.just(
+            new ResponseEntity<ErrorResponse>(
+                new ErrorResponse("ERR000", "Something went wrong..."),
+                HttpStatus.INTERNAL_SERVER_ERROR)
+        );
     }
 
 }
--- problem-domain-pd1/pd1-backend-solution-bs4/src/main/java/org/harisux/fullstackplay/pd1backendsolutionbs4/rest/FilmsApiImpl.java
+++ problem-domain-pd1/pd1-backend-solution-bs4/src/main/java/org/harisux/fullstackplay/pd1backendsolutionbs4/rest/FilmsApiImpl.java
@@ -4,6 +4,7 @@ import org.harisux.fullstackplay.openapi.api.FilmsApi;
 import org.harisux.fullstackplay.openapi.model.Film;
 import org.harisux.fullstackplay.pd1backendsolutionbs4.service.FilmsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
@@ -18,8 +19,8 @@ public class FilmsApiImpl implements FilmsApi {
     FilmsService filmsService;
 
     @Override
-    public Mono<Film> getFilm(Integer id, ServerWebExchange exchange) {
-        return filmsService.getFilm(id);
+    public Mono<ResponseEntity<Film>> getFilm(Integer id, ServerWebExchange exchange) {
+        return filmsService.getFilm(id).map(film -> ResponseEntity.ok(film));
     }
 
   
